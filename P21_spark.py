6fa100b5b2080d8f13678537bf183f218c9f1f8c
venuscaitwxj <70314178+venuscaitwxj@users.noreply.github.com>
2021-04-14 22:49:09 +0800

    Add files via upload

--- P21_spark.py
+++ P21_spark.py
@@ -2,14 +2,17 @@ from pyspark import SparkConf, SparkContext
 
 import string
 
+pattern = sys.argv[1]
+
 conf = SparkConf().setMaster('local').setAppName('DistributedGrep')
 sc = SparkContext(conf = conf)
 
 lines = sc.textFile("input.txt")
-pythonLines = lines.filter(lambda line : 'Web' in line)
-print (pythonLines + "\n")
+pythonLines = lines.filter(lambda line : pattern in line)
+#print(pythonLines + "\n")
 #lst = pythonLines.collect()
 
 
 #for ele in lst:
 #    print ele
+pythonLines.saveAsTextF1ile("output.txt")
